404f825aebf5f79d15b0ae8e2c6cd40500a01531
Danilo <danilo.colloca91@gmail.com>
2021-05-14 00:50:50 +0200

    revolut img

--- revolut/js/script.js
+++ revolut/js/script.js
@@ -8,30 +8,9 @@ $('.drop-down-container').click(function(){
 /*aggiungendo la funzione hover in accoppiata alla funzione click otteniamo lo stesso comportamento dei menu originali*/
 $('.drop-down-container').hover(
   function(){
-    //mette in focus lo span che attiva il dropdown
-    $(this).find('span').addClass('font-black');
-    //mostra il menu
     $(this).find('.dropDown-menu').removeClass('hide');
-
   }, function(){
-    //rimuove il focus dallo span che attiva il dropdown
-    $(this).find('span').removeClass('font-black');
-    //nasconde il menu
     $(this).find('.dropDown-menu').addClass('hide');
+
   }
 );
-
-
-
-//al click mette in focus la sezione personal o business
-$('.go-to-area > a.personalArea').click(
-  function(){
-    $(this).addClass('font-black').next().removeClass('font-black');
-
-});
-
-$('.go-to-area > a.businessArea').click(
-  function(){
-    $(this).addClass('font-black').prev().removeClass('font-black');
-
-});
